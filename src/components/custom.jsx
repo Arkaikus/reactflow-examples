c84e9cd2bdf984eb7edb17072714bcb35704edde
Santiago G. Alegria <santiago.giraldo@submittable.com>
2024-08-09 15:59:23 -0500

    feat: add xyflow and add a quickstart page with custom nodes

---
+++ src/components/custom.jsx
@@ -0,0 +1,39 @@
+import React, { memo, useState } from 'react';
+import { Handle, Position, NodeToolbar, NodeResizer } from '@xyflow/react';
+
+function CustomNode({ data }) {
+    const [selected, setSelected] = useState(false)
+
+    return (
+        <div className="flex items-center justify-start h-full px-4 py-2 align-middle bg-white border-2 rounded-md shadow-md border-stone-400" onClick={() => setSelected(!selected)}>
+            <NodeToolbar>
+                <button>delete</button>
+                <button>copy</button>
+                <button>expand</button>
+
+            </NodeToolbar>
+            <div className="flex">
+                <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full">
+                    {data.emoji}
+                </div>
+                <div className="ml-2">
+                    <div className="text-lg font-bold">{data.name}</div>
+                    <div className="text-gray-500">{data.job}</div>
+                </div>
+            </div>
+            {selected && <NodeResizer minWidth={100} minHeight={30} />}
+            <Handle
+                type="target"
+                position={Position.Top}
+                className="w-16 !bg-teal-500"
+            />
+            <Handle
+                type="source"
+                position={Position.Bottom}
+                className="w-16 !bg-teal-500"
+            />
+        </div>
+    );
+}
+
+export default memo(CustomNode);
