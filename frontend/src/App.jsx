ed391cfb70b1e81058a0d95ff0a9f8e1ce3aefdc
Santiago G. Alegria <santiago.giraldo@submittable.com>
2024-08-26 13:06:44 -0500

    fix: move root to frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import NotFound from './components/404';
+import Quickstart from './components/quickstart';
 import './styles/App.css';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="*" element={<Navigate to="/404" />} />
+          <Route path="*" element={<Navigate to="/quickstart" />} />
+          <Route path="/quickstart" element={<Quickstart />} />
           <Route path="/404" element={<NotFound />} />
         </Routes>
       </div>
